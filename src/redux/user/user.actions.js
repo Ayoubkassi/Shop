620e76dc593b6875d8b080ac02de97f687aa8bad
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2021-08-04 21:44:40 +0100

    add Redirect and User Action

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,4 +1,7 @@
+import { UserActionsTypes } from './user.types';
+
+
 export const setCurrentUser = user => ({
-  type : 'SET_CURRENT_USER',
+  type : UserActionsTypes.SET_CURRENT_USER,
   payload : user
 });
--- src/redux/user/user.reducer.jsx
+++ src/redux/user/user.reducer.jsx
@@ -1,10 +1,12 @@
+import { UserActionsTypes } from './user.types';
+
 const INITIAL_STATE = {
   currentUser : null
 }
 
 const userReducer = (state = INITIAL_STATE,action) => {
   switch(action.type){
-    case 'SET_CURRENT_USER':
+    case UserActionsTypes.SET_CURRENT_USER:
       return{
         ...state,
         currentUser : action.payload
